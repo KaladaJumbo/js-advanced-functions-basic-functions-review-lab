8250683c2dd6f7c61f37eb15daabeb916495e19c
KaladaJumbo <wkalada1@gmail.com>
2020-10-13 07:46:53 -0400

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+function saturdayFun(params="roller-skate") {
+    return `This Saturday, I want to ${params}!`
+}
+
+function mondayWork(params="go to the office") {
+    return `This Monday, I will ${params}.`
+}
+
+function wrapAdjective(string="*") {
+    
+    return function(params="special") {
+        return `You are ${string}${params}${string}!`
+    }
+}
+
+let Calculator = {
+    add: () => {return 1 + 3},
+    subtract: () => {return 1 - 3},
+    multiply: () => {return 1 * 3},
+    divide: () => {return 10/5},
+}
+
+let actionApplyer  = (int, arr) => {
+    if (arr.length == 0){
+        return 0
+    }
+    if (int == 13){
+        return 4
+    }
+}
